0c1713910c99e2748e95567605663e601bf2ae29
Sam Thi Thuy <thuymieu44@gmail.com>
2026-05-22 10:40:09 +0900

    Add files via upload

---
+++ ex 6/main_6.js
@@ -0,0 +1,4 @@
+let math= Number(prompt('Nhập điểm môn Toán:'));
+let physics= Number(prompt('Nhập điểm Vật Lý:'));
+let chemistry= Number(prompt('Nhập điểm Hóa Học:'));
+alert('Điểm TB=' + ((math + physics + chemistry) / 3));
